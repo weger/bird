12618dda28755d16e0a437ffc1553edcb9cb64f3
felix <felixyuebin@gmail.com>
2013-05-06 16:59:21 +0800

    jsp文件简单支持，修复流媒体支持bug

--- config.js
+++ config.js
@@ -4,7 +4,11 @@ exports.Server = {
         //静态文件根目录
         "basePath": "D:/workspace/hrlms/web",
         //忽略的静态文件请求，与此正则匹配的请求将直接走转发规则（可选配置）
-        "ignoreRegExp":  /\/js\/urls\.js|\/js\/urlst\.js/g
+        "ignoreRegExp":  /\/js\/urls\.js/g
+    },
+    "platform.baidu.com": {
+        //静态文件根目录
+        "basePath": "D:/workspace/platform_new"
     },
     "peixun.baidu.com": {
         "basePath": "D:/workspace/peixun-planA-userweb"
@@ -30,6 +34,24 @@ exports.TranspondRules = {
                 //"port": "8045",
                 //"attachHeaders": {"app-id": 5},
                 "path": "/hrlms/rs"
+            }
+        }
+    },
+    "platform.baidu.com": {
+        //目标服务器的ip和端口，域名也可，但注意不要被host了
+        targetServer: {
+            "host": "10.44.67.14",
+            "port": "8045"
+        },
+        //特殊请求转发，可选配置，内部的host、port和attachHeaders为可选参数
+        regExpPath: {
+            "/platform/rs": {
+                "path": "/platform/rs"
+            },
+            "/platform": {
+                "host": "platform.baidu.com",
+                "port": "80",
+                "path": "/"
             }
         }
     },
